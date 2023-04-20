7c28c8c4f41fef78920e11231d02fe920e9c16ed
jsiudowski <113116921+jsiudowski@users.noreply.github.com>
2023-04-20 11:26:41 -0400

    Update main.js

--- WebExample/main.js
+++ WebExample/main.js
@@ -1,3 +1,23 @@
+customElements.define('my-paragraph',
+    class extends HTMLElement {
+        constructor() {
+            super();
+                              
+            const template = document.getElementById('my-paragraph');
+            const templateContent = template.content;
+                              
+            this.attachShadow({mode: 'open'}).appendChild(
+                 templateContent.cloneNode(true)
+            );
+        }
+    }
+);
+
+const slottedSpan = document.querySelector('my=paragraph span');
+
+console.log(slottedSpan.assignedSlot);
+console.log(slottedSpan.slot);
+
 // Create a class for the element
 class WordCount extends HTMLParagraphElement {
     constructor() {

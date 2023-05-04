f4ebeb41842185b57f448a9cbd8f91303f497a7d
jsiudowski <113116921+jsiudowski@users.noreply.github.com>
2023-05-04 12:04:37 -0400

    Fixed todo.js, helped count function work and add/remvoe work

--- Assignment3_JS/todo.js
+++ Assignment3_JS/todo.js
@@ -1,44 +1,43 @@
+// Starts at 1 because there is a default list item at the start of the list.
+var countOfItems = 1;
+            function addItem() {
+                // Input Val
+                var newItem = document.getElementById("newItem").value;
 
+                //Create List
+                var li = document.createElement("li");
+                li.innerText = newItem;
 
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("nextToDo").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+                // X button
+                var button = document.createElement("button");
+                button.innerText = "X";
+                button.onclick = function() {
+                removeItem(this);
+                }
+                li.appendChild(button);
 
-    if (inputValue === '') {
-      alert("You must write something!");
-    } else {
-      document.getElementById("my_list").appendChild(li);
-    }
-    document.getElementById("nextToDo").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+                // Add Val, modifies count too
+                document.getElementById("list").appendChild(li);
+                countOfItems++;
+                countItems();
 
-    for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
-          var div = this.parentElement;
-          div.style.display = "none";
-        }
-    }
-}
+                // Clear 
+                document.getElementById("newItem").value = "";
+            }
 
-var myToDoList = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myToDoList.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+            // Removes item when X is clicked, modifies Count too
+            function removeItem(item) {
+                // Remove from List
+                item.parentElement.remove();
+                countOfItems--;
+                countItems();
+            }
 
-// var inputCount = document.getElementsByTagName("LI");
-// var count = document.getElementById("countToDo");
-// let heading = document.createElement("h1");
-// count.appendChild(heading);
+            // Counts items in list and sets the paragraph element to the value
+            function countItems() {
+                var par = document.querySelector("#countNum");
+                par.innerText = countOfItems;
+            }
+
+            // Call it to initialize the count at the beginning of the application
+            countItems();

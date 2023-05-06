be82e693e5593f22e7d4d4ba377c6ec8f4a3820f
jsiudowski <113116921+jsiudowski@users.noreply.github.com>
2023-05-06 18:43:03 -0400

    Finished adding Enter button press check for add

--- Assignment3_JS/todo.js
+++ Assignment3_JS/todo.js
@@ -1,43 +1,58 @@
 // Starts at 1 because there is a default list item at the start of the list.
 var countOfItems = 1;
-            function addItem() {
-                // Input Val
-                var newItem = document.getElementById("newItem").value;
+            
+function addItem() {
+    // The value inside of the input textbox
+    var newItem = document.getElementById("newItem").value;
 
-                //Create List
-                var li = document.createElement("li");
-                li.innerText = newItem;
+    //Create List elements
+    var li = document.createElement("li");
+    li.innerText = newItem;
 
-                // X button
-                var button = document.createElement("button");
-                button.innerText = "X";
-                button.onclick = function() {
-                removeItem(this);
-                }
-                li.appendChild(button);
+    // X button on each of the items in the list
+    var button = document.createElement("button");
+    button.innerText = "X";
+    button.onclick = function() {
+        removeItem(this);
+    }
+    li.appendChild(button);
 
-                // Add Val, modifies count too
-                document.getElementById("list").appendChild(li);
-                countOfItems++;
-                countItems();
+    // Add Value, modifies count too
+    document.getElementById("list").appendChild(li);
+    countOfItems++;
+    countItems();
 
-                // Clear 
-                document.getElementById("newItem").value = "";
-            }
+    // Clears textbox input
+    document.getElementById("newItem").value = "";
+}
 
-            // Removes item when X is clicked, modifies Count too
-            function removeItem(item) {
-                // Remove from List
-                item.parentElement.remove();
-                countOfItems--;
-                countItems();
-            }
+// Removes item when X is clicked, modifies Count too
+function removeItem(item) {
+    // Remove from List
+    item.parentElement.remove();
+    countOfItems--;
+    countItems();
+}
 
-            // Counts items in list and sets the paragraph element to the value
-            function countItems() {
-                var par = document.querySelector("#countNum");
-                par.innerText = countOfItems;
-            }
+// Counts items in list and sets the paragraph element to the value
+function countItems() {
+    var par = document.querySelector("#countNum");
+    par.innerText = countOfItems;
+}
 
-            // Call it to initialize the count at the beginning of the application
-            countItems();
+// Adds Event listener to enter button on text box, enter and clicking add therefore do
+// the same thing.
+var input = document.getElementById("newItem");
+// Listener that activates when the user presses a key on the keyboard
+input.addEventListener("keypress", function(event) {
+    // Checking for the "Enter" key press
+    if (event.key === "Enter") {
+        // Cancel the default action
+        event.preventDefault();
+        // Trigger the addBtn functionality and addItem() function
+        document.getElementById("addBtn").click();
+    }
+});
+
+// Call it to initialize the count at the beginning of the application
+countItems();
